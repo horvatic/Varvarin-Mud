ae63a62733925b7a24d7951c2f3167cfec44db38
Shawn Horvatic <sbhorvatic@gmail.com>
2018-12-12 22:26:12 -0600

    Basic communcation for client and server

---
+++ src/client/Varvarin.Client/Varvarin.Ui/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Varvarin.Ui
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var processor = new Process();
+            processor.Start();
+
+        }
+    }
+}
---
+++ src/server/Varvarin.Server/Varvarin.Api/Startup.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Varvarin.Api
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            var webSocketOptions = new WebSocketOptions()
+            {
+                KeepAliveInterval = TimeSpan.FromSeconds(120),
+                ReceiveBufferSize = 4 * 1024
+            };
+            app.UseWebSockets(webSocketOptions);
+            var messageQueues = new ConcurrentDictionary<Guid, ConcurrentQueue<string>>();
+            app.Use(async (context, next) =>
+            {
+                if (context.WebSockets.IsWebSocketRequest)
+                {
+                    var guid = Guid.NewGuid();
+                    messageQueues.GetOrAdd(guid, new ConcurrentQueue<string>());
+                    new Processor(guid, messageQueues).Add(await context.WebSockets.AcceptWebSocketAsync());
+                }
+                else
+                {
+                    context.Response.StatusCode = 400;
+                }
+            });
+        }
+    }
+}
